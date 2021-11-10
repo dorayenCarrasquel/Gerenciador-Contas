d1ae624724c5abbe9f2ad525846a04c2e94fc269
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-10 16:11:24 -0300

    Modificar configuaracao do Banco de dados

--- src/main/java/br/com/zup/GerenciadorDeContas/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/ContaRepository.java
@@ -2,6 +2,6 @@ package br.com.zup.GerenciadorDeContas;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ContaRepository extends CrudRepository<Conta,String> {
+public interface ContaRepository extends CrudRepository<Conta,Integer> {
 
 }

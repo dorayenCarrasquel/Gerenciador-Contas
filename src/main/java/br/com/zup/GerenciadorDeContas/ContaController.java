1902f231548e45491c2958478a88490d6fd4c1f7
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-10 18:55:20 -0300

    Mapear exibir lista de contas

--- src/main/java/br/com/zup/GerenciadorDeContas/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/ContaController.java
@@ -2,14 +2,19 @@ package br.com.zup.GerenciadorDeContas;
 
 import br.com.zup.GerenciadorDeContas.DTOS.ContaCadastroDto;
 import br.com.zup.GerenciadorDeContas.DTOS.ContaRespostaDto;
+import br.com.zup.GerenciadorDeContas.DTOS.ResumoDto;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping ("/contas")
@@ -28,5 +33,19 @@ public class ContaController {
         ContaRespostaDto contaRespostaDto = modelMapper.map(conta, ContaRespostaDto.class);
         return contaRespostaDto;
     }
+
+    @GetMapping
+    public List<ResumoDto> exibirLista(){
+        List<ResumoDto>listaDeExibicao = new ArrayList<>();
+        for(Conta conta:contaService.exibirTodasAsContas()){
+            listaDeExibicao.add(new ResumoDto(conta.getId(),conta.getNome(),conta.getValor(),conta.getStatus()));
+        }
+        return listaDeExibicao;
+
+    }
+
+
+
+
 
 }

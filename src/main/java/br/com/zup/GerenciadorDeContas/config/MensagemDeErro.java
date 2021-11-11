281a2435e00cd420b9f58da88c063ed3d51631ee
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-11 18:39:07 -0300

    Criar Classe mensagem de erro

---
+++ src/main/java/br/com/zup/GerenciadorDeContas/config/MensagemDeErro.java
@@ -0,0 +1,18 @@
+package br.com.zup.GerenciadorDeContas.config;
+
+public class MensagemDeErro {
+    private String mensagem;
+
+    public MensagemDeErro(String mensagem) {
+        this.mensagem = mensagem;
+
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}

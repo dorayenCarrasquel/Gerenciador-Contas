7297d24ba66479473c8239711057f586d63b7990
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-11 18:43:49 -0300

    Manipular Excepcoes de validacoes

--- src/main/java/br/com/zup/GerenciadorDeContas/config/ControladorAdvisor.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/config/ControladorAdvisor.java
@@ -2,9 +2,14 @@ package br.com.zup.GerenciadorDeContas.config;
 
 import br.com.zup.GerenciadorDeContas.Exceptions.IdNaoEncontradoException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ControladorAdvisor {
@@ -14,6 +19,16 @@ public class ControladorAdvisor {
     public MensagemDeErro manipularIdNaoEncontrado(IdNaoEncontradoException exception){
         return new MensagemDeErro(exception.getMessage());
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<MensagemDeErro> manipularExcecoesDeValidacao(MethodArgumentNotValidException exception){
+        List<MensagemDeErro> listamensagen = new ArrayList<>();
 
+        for (FieldError fieldError : exception.getFieldErrors()){
+            listamensagen.add(new MensagemDeErro(fieldError.getDefaultMessage()));
+        }
+
+        return listamensagen;
+    }
 
 }

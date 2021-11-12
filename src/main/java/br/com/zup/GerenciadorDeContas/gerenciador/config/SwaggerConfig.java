0798364b8fea45174e3c3cd3e3b863a026676e66
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-12 18:20:47 -0300

    Configurar Swagger

--- src/main/java/br/com/zup/GerenciadorDeContas/gerenciador/config/SwaggerConfig.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/gerenciador/config/SwaggerConfig.java
@@ -1,13 +1,21 @@
-package br.com.zup.GerenciadorDeContas.gerenciador;
+package br.com.zup.GerenciadorDeContas.gerenciador.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-    @Configuration
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
     @EnableSwagger2
     public class SwaggerConfig {
         @Bean
@@ -16,7 +24,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
                     .select()
                     .apis(RequestHandlerSelectors.any())
                     .paths(PathSelectors.any())
-                    .build();
+                    .build()
+                    .useDefaultResponseMessages(false)
+                    .globalResponseMessage(RequestMethod.GET, responseMessageForGET());
         }
+
+        private List<ResponseMessage> responseMessageForGET()
+        {
+            return new ArrayList<>() {{
+                add(new ResponseMessageBuilder()
+                        .code(500)
+                        .message("500 message")
+                        .responseModel(new ModelRef("Foi gerada uma exceção"))
+                        .build());
+                add(new ResponseMessageBuilder()
+                        .code(403)
+                        .message("Não tem permissão para acessar este recurso")
+                        .build());
+            }};
+        }
+
+
+
+
+
     }
 
